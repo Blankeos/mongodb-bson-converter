6c97c86bfafa0dfa0067b53e4c79884a60216b75
Blankeos <carloantonioct@gmail.com>
2024-05-10 02:16:13 +0800

    feat: Added working parser.

---
+++ src/lib/scan.ts
@@ -0,0 +1,104 @@
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export function scan(obj: { [key: string]: any }, depth: number = 0): string {
+  let output = '';
+  let k;
+
+  if (!(obj instanceof Object)) throw Error(`${obj} is not an object.`);
+
+  const rootIsObject =
+    depth === 0 && typeof obj === 'object' && !Array.isArray(obj) && obj !== null;
+  // Handle if Root object.
+  if (rootIsObject) {
+    // Root is an object
+    output += '{\n';
+    depth += 1;
+  }
+
+  for (k in obj) {
+    const isLast = k == Object.keys(obj).at(-1);
+
+    if (obj.hasOwnProperty(k)) {
+      // Indent
+      output += '  '.repeat(depth);
+
+      // Parse null
+      if (obj[k] === null) {
+        output += `${k}: null`;
+
+        if (!isLast) output += ',';
+        output += '\n';
+        continue;
+      }
+
+      // Parse boolean
+      if (typeof obj[k] === 'boolean') {
+        output += `${k}: ${obj[k]}`;
+
+        if (!isLast) output += ',';
+        output += '\n';
+        continue;
+      }
+
+      // Parse $date
+      if (obj[k].hasOwnProperty('$date')) {
+        if (obj[k]['$date'].hasOwnProperty('$numberLong')) {
+          output += `${k}: new Date(${obj[k]['$date']['$numberLong']})`;
+        } else if (obj[k]['$date'].hasOwnProperty('$numberInt')) {
+          output += `${k}: new Date(${obj[k]['$date']['$numberInt']})`;
+        }
+
+        if (!isLast) output += ',';
+        output += '\n';
+        continue;
+      }
+
+      // Parse $oid
+      if (obj[k].hasOwnProperty('$oid')) {
+        output += `${k}: ObjectId("${obj[k]['$oid']}")`;
+
+        if (!isLast) output += ',';
+        output += '\n';
+        continue;
+      }
+
+      // Parse $numberInt
+      if (obj[k].hasOwnProperty('$numberInt')) {
+        output += `${k}: ${obj[k]['$numberInt']}`;
+        if (!isLast) output += ',';
+        output += '\n';
+        continue;
+      }
+
+      // (identifier key) or (0) Beginning (an array)
+      if (!isNaN(parseInt(k))) {
+        output += `[`;
+      } else {
+        output += `${k}: `;
+      }
+
+      if (obj[k] instanceof Object) {
+        output += '{\n';
+        output += scan(obj[k], depth + 1);
+        output += '  '.repeat(depth) + '}';
+      } else {
+        output += `"${obj[k]}"`;
+      }
+
+      if (!isNaN(parseInt(k))) output += `]`;
+    }
+
+    // Line Endings
+    if (!isLast) output += ',';
+    output += '\n';
+  }
+
+  // Handle if Root object.
+  if (rootIsObject) {
+    // Root is an object
+    output += '}';
+  }
+
+  return output;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,108 @@
-<script>
+<script lang="ts">
+  import { debounce } from '@/lib/debounce';
+  import { scan } from '@/lib/scan';
+
+  // Compile the Typebox schema
+  const isValidJSON = (str: string) => {
+    try {
+      JSON.parse(str);
+    } catch (e) {
+      return false;
+    }
+    return true;
+  };
+
+  let bsonValue = $state('');
+  let isValidating = $state(false);
+  let isValid = $state(false);
+  let convertedValue = $state('');
+  let touched = $state(false);
+
+  let copied = $state(false);
+
+  const validate = debounce((str: string) => {
+    isValid = isValidJSON(str);
+    isValidating = false;
+  }, 500);
+
+  $effect(() => {
+    isValidating = true;
+    validate(bsonValue);
+  });
+
+  $effect(() => {
+    if (isValid) {
+      convertedValue = scan(JSON.parse(bsonValue));
+    }
+  });
 </script>
 
 <main class="h-full min-h-screen bg-slateblue">
   <div class="mx-auto w-full max-w-lg px-7">
     <!--  -->
-    <header class="flex flex-col items-center py-2 pt-5">
-      <h1 class="text-white">MongoDB BSON Converter</h1>
+    <header class="flex flex-col items-center gap-y-1 py-2 pt-5">
+      <h1 class="font-medium text-white">MongoDB BSON Converter</h1>
+      <p class="text-center text-sm text-gray-500">
+        Copy a BSON from MongoDB Atlas, paste it here and get a valid query that will work with
+        <code class="code rounded-md bg-gray-800 px-1.5 py-0.5 text-gray-400">insertOne()</code> or
+        <code class="code rounded-md bg-gray-800 px-1.5 py-0.5 text-gray-400">insertMany()</code>
+      </p>
       <p class="text-sm text-white">
-        Made by <a class="text-spring opacity-90" target="_blank" href="https://carlo.vercel.app/"
-          >Carlo</a
+        Made with 💚 by <a
+          class="text-spring opacity-90"
+          target="_blank"
+          href="https://carlo.vercel.app/">Carlo</a
         >
       </p>
     </header>
 
     <div class="h-12" />
 
-    <textarea class="textarea textarea-bordered min-h-72 w-full text-xs"></textarea>
-    <textarea class="textarea textarea-bordered min-h-72 w-full text-xs"></textarea>
+    <textarea
+      onfocus={() => (touched = true)}
+      class="textarea textarea-bordered min-h-72 w-full text-xs"
+      bind:value={bsonValue}
+    ></textarea>
+
+    <div class="flex h-12 items-center gap-x-2">
+      {#if isValidating}
+        <span class="loading loading-infinity loading-md"></span>
+        <span class="text-sm">Validating BSON...</span>
+      {:else if touched && !isValid && bsonValue !== ''}
+        <span class="text-sm"> 🤕 Not a valid BSON/JSON.</span>
+      {/if}
+    </div>
+
+    <div class="relative">
+      {#if convertedValue !== ''}
+        <button
+          class="btn btn-sm absolute right-2 top-2"
+          onclick={() => {
+            navigator.clipboard.writeText(convertedValue);
+            copied = true;
+            setTimeout(() => {
+              copied = false;
+            }, 5000);
+          }}
+          onmouseleave={() => {
+            setTimeout(() => {
+              copied = false;
+            }, 1500);
+          }}
+        >
+          {#if copied}
+            ✅ Copied
+          {:else}
+            Copy
+          {/if}</button
+        >
+      {/if}
+
+      <textarea
+        class="textarea textarea-bordered min-h-72 w-full text-xs"
+        readonly
+        value={convertedValue}
+      ></textarea>
+    </div>
   </div>
 </main>
