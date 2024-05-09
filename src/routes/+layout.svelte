79129dab89cd0f1cc4ad59d6fb73cf50d783110a
Blankeos <carloantonioct@gmail.com>
2024-05-10 02:32:06 +0800

    feat: Added SEO?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script>
-    import "@/styles/globals.css";
-    let { children } =  $props();
+  import '@/styles/globals.css';
+  let { children } = $props();
 </script>
 
+<svelte:head>
+  <title>MongoDB BSON Converter</title>
+  <meta charset="UTF-8" />
+  <meta
+    name="description"
+    content="Convert BSON from MongoDB Atlas to a valid JSON for insertOne() or insertMany()"
+  />
+  <meta name="keywords" content="mongodb,atlas,bson,json,converter" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="author" content="Carlo Taleon" />
+  <meta name="robots" content="index, follow" />
+</svelte:head>
 {@render children()}
